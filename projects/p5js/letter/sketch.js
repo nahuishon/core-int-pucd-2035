bd890cece31669ab8fc67fc3086cf26b1efd252a
nahuishon <shonn620@newschool.edu>
2020-11-10 19:41:02 +0900

    Update sketch.js

--- projects/p5js/letter/sketch.js
+++ projects/p5js/letter/sketch.js
@@ -41,7 +41,7 @@ function setup() {
     const words = paragraph.split(' ');
     for (let j = 0; j < words.length; j++) {
       const spannedWord = createSpan(words[j]);
-      const dw = new DanceSpan(spannedWord, random(700), random(300));
+      const dw = new DanceSpan(spannedWord, random(1000), random(200));
       dancingWords.push(dw);
     }
   }
